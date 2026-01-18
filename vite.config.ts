7f1f88bb15b910bc4271b1911140c7c6c209350f
Sahibzada Hamza sardar <71062903+hamzoo124@users.noreply.github.com>
2026-01-18 19:57:28 +0500

    fix3

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "./",
-  // base:process.env.VITE_BASE_PATH || "/FurniSaudi",
+
+  // ✅ one base, dynamic
+  base: mode === "production"
+    ? process.env.VITE_SUPABASE_URL || "/FurniSaudi/"
+    : "/",
+
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     hmr: {
-      overlay: false
-    }
-  }
-})
+      overlay: false,
+    },
+  },
+}));
