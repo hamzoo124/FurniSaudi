980f8e952b417c64d0b410a98a7c9a126aabd3db
Sahibzada Hamza sardar <71062903+hamzoo124@users.noreply.github.com>
2026-01-18 19:25:57 +0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base:process.env.VITE_SUPABASE_UR || "/FurniSaudi",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
